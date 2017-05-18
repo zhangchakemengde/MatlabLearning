d75ade423596c1129e63a6faa4c8c18206491168
zhangchakemengde <2636213416@qq.com>
2017-05-18 19:19:12 +0800

    title
    English to Chiese

--- tempConv.m
+++ tempConv.m
@@ -1,4 +1,4 @@
-% ÎÂ¶È×ª»»
+% temperature transformation
 temp_h = input('please enter an initial temperature:\n');
 temp_k = (5/9)*temp_h+273.15;
 fprintf('%6.2f Fahrenheit equals %6.2f Kelvin temperature\n',temp_h,temp_k);
